eaf5baa6bb6eee2c71db8663bb5b5c69940b0660
YanushanK <165895044+YanushanK@users.noreply.github.com>
2025-09-11 23:21:03 +0530

    Add files via upload
    add dietitian feedback page

--- resources/views/dietitianDashboard/feedback.blade.php
+++ resources/views/dietitianDashboard/feedback.blade.php
@@ -1,6 +1,4 @@
 @extends('dietitianDashboard.layout')
-
 @section('content')
-
-
+  @include('components.feedback-table', ['items' => $items, 'title' => 'Feedback I Received', 'theme' => 'dietitian'])
 @endsection
